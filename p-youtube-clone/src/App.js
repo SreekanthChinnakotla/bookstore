36bea536764982fc51372953d3250462bf302248
SreekanthChinnakotla <sreekanthchandra4@gmail.com>
2025-08-12 17:41:34 +0530

    message

--- p-youtube-clone/src/App.js
+++ p-youtube-clone/src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <h1>hi world</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.ki
         </p>
         <a
           className="App-link"
