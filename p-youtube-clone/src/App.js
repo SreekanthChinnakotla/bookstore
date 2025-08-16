6e98e0179559222173c09bdff0d6172c06297189
SreekanthChinnakotla <sreekanthchandra4@gmail.com>
2025-08-16 21:33:05 +0530

    main page optimized

--- p-youtube-clone/src/App.js
+++ p-youtube-clone/src/App.js
@@ -1,12 +1,13 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hi world</h1>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.ki
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
